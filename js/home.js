1c03d3895491f334c055ccc943900aba50372c93
haempebe <hamidzanpranajanalabhamkerti@gmail.com>
2023-03-28 10:05:08 +0700

    sposto

--- js/home.js
+++ js/home.js
@@ -37,3 +37,22 @@ checkme.addEventListener("click", function () {
     }
 })
 
+$(document).ready(function() {
+	$(".bg-loader").hide();
+})
+
+
+
+const post = document.querySelector('div');
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        div.removeClass('');
+     } else {
+        div.removeClass('row');
+    }
+}
+          
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
